85bfcfb9b3aabc1e1744e5e17b02225e9d5ab53c
Gosia <mbas@gumed.edu.pl>
2022-02-16 10:40:56 +0100

    zmiany w skrypcie w zadaniu 3

--- NPAS-data/zad3.R
+++ NPAS-data/zad3.R
@@ -1,7 +1,7 @@
 library("tidyverse")
 library("psych")
 
-npas <- read_delim("NPAS-data01.csv", ";")
+npas <- read_tsv("data2.csv")
 # npas <- read_tsv("data.csv")
 problems(npas)
 
@@ -9,17 +9,29 @@ problems(npas)
 npas <- npas %>%
 	mutate(npas_total = rowSums(.[1:26]))
 
-#zrekoduj TIPI i zrób total
-npas <- npas %>%
-	mutate(TIPI6r = recode(TIPI6, '1'=7, '2'=6, '3'=5, '4'=4, '5'=3, '6'=2, '7'=1)) %>%
-	mutate(TIPIe = TIPI1 + TIPI6r)
- 
-glimpse(npas)
+#funkcje
+my_reverse <- function(item, min, max) {
+  max - (item - min)
+}
+reverse_items <- function(df, items_to_reverse, min, max) {
+  for(i in items_to_reverse) {
+    df[[i]] <- my_reverse(df[[i]], min, max)
+  }
+  return(df)
+}
 
-cat(" ", " ", sep = "\n")
+#zrekoduj TIPI i zrób total - ekstrawersja
+npas <- reverse_items(npas, c("TIPI6", "TIPI2", "TIPI8", "TIPI4", "TIPI10"), 1, 7)
 
-print("KORELACJA Z NERDY")
-cor.test(npas$npas_total, npas$nerdy)
+#zrekoduj TIPI i zrób total - ekstrawersja
+cor(npas$TIPI1, npas$TIPI6)
+cor(npas$TIPI2, npas$TIPI7)
+cor(npas$TIPI3, npas$TIPI8)
+cor(npas$TIPI4, npas$TIPI9)
+cor(npas$TIPI5, npas$TIPI10)
 
-print("KORELACJA TIPI")
-cor.test(npas$npas_total, npas$TIPIe)
+npas$E <- npas$TIPI1 + npas$TIPI6
+npas(npas$TIPI2, npas$TIPI7)
+npas(npas$TIPI3, npas$TIPI8)
+npas(npas$TIPI4, npas$TIPI9)
+npas(npas$TIPI5, npas$TIPI10)
